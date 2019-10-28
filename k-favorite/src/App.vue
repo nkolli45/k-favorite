ac5b9c072a775f9e3a6f7e4ce80886ff9d065e1b
nkolli1 <naviinkolli@gmail.co>
2019-10-27 23:02:03 -0400

    first commit

---
+++ k-favorite/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{ now }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello Vue",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      now: new Date()
+    };
+  },
+  methods: {
+    updateDate() {
+      this.now = new Date();
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.updateDate();
+    }, 100);
+  }
+};
+</script>
